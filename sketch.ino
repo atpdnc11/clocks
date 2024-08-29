ad1426728ff897c574608b66d2c8f757714d6b0b
arvind patil <atpdnc11@gmail.com>
2024-08-29 10:41:43 +0530

    Add files via upload

--- sketch.ino
+++ sketch.ino
@@ -1,86 +1,65 @@
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
-#include <TimeLib.h>
+#include <Arduino.h>
+#include <U8g2lib.h> // u8g2 library for drawing on OLED display - needs to be installed in Arduino IDE first
+#include <Wire.h> // wire library for IIC communication with the OLED display
+#include <RTClib.h> // RTC library for real-time clock
 
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+//U8G2_SSD1306_128X64_NONAME_1_HW_I2C u8g2(U8G2_R0); // set the OLED display to 128x64px, HW IIC, no rotation, used in WOKWI
+U8G2_SH1107_128X128_1_HW_I2C u8g2(U8G2_R0); // final display, 128x128px [page buffer, size = 128 bytes], HW IIC connection
 
-#define OLED_RESET    -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+RTC_DS3231 rtc; // create an RTC object
 
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+// IIC connection of the OLED display and Arduino UNO
+// +5V > +5V
+// GND > GND
+// SCL (serial clock) > A5 or SCL
+// SDA (serial data) > A4 or SDA
 
-int clockCenterX = SCREEN_WIDTH / 2;
-int clockCenterY = ((SCREEN_HEIGHT - 16) / 2) + 16; // top yellow part is 16 px height
-int clockRadius = 33; // Increased radius by 10 pixels
+void setup(void) {
+  u8g2.begin();  // begin the u8g2 library
+  u8g2.setContrast(255); // set display contrast/brightness
 
-void setup() {
-  Serial.begin(115200);
-  Serial.println();
-
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
-    Serial.println(F("SSD1306 allocation failed"));
-    for(;;);
+  if (!rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    while (1);
   }
 
-  display.display(); // Clear the buffer
-  delay(2000);  // Pause for 2 seconds
-  display.clearDisplay();   // clears the screen and buffer
-  display.setTextColor(SSD1306_WHITE); // Draw white text
-  display.setTextSize(1);      // Normal 1:1 pixel scale
-  display.setRotation(0);  // Rotate screen 0 degrees (portrait mode)
-}
-
-void loop() {
-  display.clearDisplay();   // clears the screen and buffer
-
-  // Draw clock
-  analogClockFrame();
-
-  // Print text
-  printText("Arvind", 0, 10); // Adjust the position as needed
-
-  display.display();  // Display the content
-  delay(1000);  // Pause for a second
-}
-
-void analogClockFrame() {
-  // Draw the clock face
-  display.drawCircle(clockCenterX, clockCenterY, 2, SSD1306_WHITE);
-  
-  // Hour ticks
-  for (int z = 0; z < 360; z += 30) {
-    float angle = z;
-    angle = (angle / 57.29577951); // Convert degrees to radians
-    int x2 = (clockCenterX + (sin(angle) * clockRadius));
-    int y2 = (clockCenterY - (cos(angle) * clockRadius));
-    int x3 = (clockCenterX + (sin(angle) * (clockRadius - (clockRadius / 8))));
-    int y3 = (clockCenterY - (cos(angle) * (clockRadius - (clockRadius / 8))));
-    display.drawLine(x2, y2, x3, y3, SSD1306_WHITE);
+  if (rtc.lostPower()) {
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__))); // set RTC to the date & time this sketch was compiled
   }
-
-  // Display second hand
-  float angle = second() * 6;
-  angle = (angle / 57.29577951); // Convert degrees to radians
-  int x3 = (clockCenterX + (sin(angle) * (clockRadius - (clockRadius / 5))));
-  int y3 = (clockCenterY - (cos(angle) * (clockRadius - (clockRadius / 5))));
-  display.drawLine(clockCenterX, clockCenterY, x3, y3, SSD1306_WHITE);
-
-  // Display minute hand
-  angle = minute() * 6;
-  angle = (angle / 57.29577951); // Convert degrees to radians
-  x3 = (clockCenterX + (sin(angle) * (clockRadius - (clockRadius / 4))));
-  y3 = (clockCenterY - (cos(angle) * (clockRadius - (clockRadius / 4))));
-  display.drawLine(clockCenterX, clockCenterY, x3, y3, SSD1306_WHITE);
-
-  // Display hour hand
-  angle = hour() * 30 + int((minute() / 12) * 6);
-  angle = (angle / 57.29577951); // Convert degrees to radians
-  x3 = (clockCenterX + (sin(angle) * (clockRadius - (clockRadius / 2))));
-  y3 = (clockCenterY - (cos(angle) * (clockRadius - (clockRadius / 2))));
-  display.drawLine(clockCenterX, clockCenterY, x3, y3, SSD1306_WHITE);
 }
 
-void printText(String text, int x, int y) {
-  display.setCursor(x, y);
-  display.print(text);
+void loop(void) {
+  DateTime now = rtc.now(); // get the current date and time
+
+  int hours = now.hour();
+  int minutes = now.minute();
+  int seconds = now.second();
+
+  // Calculate angles
+  float hourAngle = (hours % 12 + minutes / 60.0) * 30; // 360 / 12 = 30 degrees per hour
+  float minuteAngle = (minutes + seconds / 60.0) * 6; // 360 / 60 = 6 degrees per minute
+  float secondAngle = seconds * 6; // 360 / 60 = 6 degrees per second
+
+  // Calculate hand positions
+  int hourX = 64 + 30 * cos(radians(hourAngle - 90)); // length of hour hand is 30
+  int hourY = 64 + 30 * sin(radians(hourAngle - 90));
+  int minuteX = 64 + 45 * cos(radians(minuteAngle - 90)); // length of minute hand is 45
+  int minuteY = 64 + 45 * sin(radians(minuteAngle - 90));
+  int secondX = 64 + 55 * cos(radians(secondAngle - 90)); // length of second hand is 55
+  int secondY = 64 + 55 * sin(radians(secondAngle - 90));
+
+  u8g2.firstPage(); // select the first page of the display (page is 128x8px), since we are using the page drawing method of the u8g2 library
+  do {
+    // Draw clock face
+    u8g2.drawCircle(64, 64, 60, U8G2_DRAW_ALL); // draw the outer circle of the clock
+    u8g2.drawCircle(64, 64, 2, U8G2_DRAW_ALL); // draw the center circle of the clock
+
+    // Draw clock hands
+    u8g2.drawLine(64, 64, hourX, hourY); // draw hour hand
+    u8g2.drawLine(64, 64, minuteX, minuteY); // draw minute hand
+    u8g2.drawLine(64, 64, secondX, secondY); // draw second hand
+
+  } while (u8g2.nextPage()); // go over all the pages until the whole display is updated
+
+  delay(1000); // wait for 1 second before refreshing the display
 }
